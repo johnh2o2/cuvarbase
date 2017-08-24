bc7c8db62b3a5e11a6716edffe1e819e07ef91f9
John Hoffman <jah5@princeton.edu>
2017-08-24 10:53:20 +0300

    wip adding frequency dependent bin search and autofrequency with the correct scaling

--- cuvarbase/bls.py
+++ cuvarbase/bls.py
@@ -223,3 +223,198 @@ def eebls_gpu(t, y, dy, freqs, qmin=0.01, qmax=0.5, nstreams=10,
     qphi_sols = [(qvals[b], phivals[b]) for b in bls_sols]
 
     return bls_g.get()/YY, qphi_sols
+
+
+def q_transit(freq, rho, q0=0.038285):
+    return q0 * (freq ** (4./3.)) * (rho ** (-1./3.))
+
+
+def bls_fast_autofreq(t, fmin=1E-2, fmax=1E2, oversampling=2,
+                      rho=1, qmin_fac=0.2):
+    T = max(t) - min(t)
+    freqs = [fmin]
+    while freqs[-1] < fmax:
+        df = qmin_fac * q_transit(freqs[-1], rho) / (oversampling * T)
+        freqs.append(freqs[-1] + df)
+    freqs = np.array(freqs)
+    q0vals = q_transit(freqs, rho)
+    return freqs, q0vals
+
+
+def eebls_gpu_fast(t, y, dy, freqs, rho=1, nstreams=10, qmin_fac=0.2,
+                   qmax_fac=5., noverlap=5, alpha=1.5, block_size=256,
+                   batch_size=1, plot_status=False, **kwargs):
+
+    """
+    Box-Least Squares, accelerated with PyCUDA
+
+    Parameters
+    ----------
+    t: array_like, float
+        Observation times
+    y: array_like, float
+        Observations
+    dy: array_like, float
+        Observation uncertainties
+    fmin: float, optional (default: 1E-2)
+        Minimum frequency
+    fmax: float, optional (default: 1E2)
+        Maximum frequency
+    oversampling: float, optional (default: 2)
+        Factor that enhances frequency spacing relative
+        to fiducial spacing
+    rho: float, optional (default: 1)
+        Stellar density in solar units
+    nstreams: int, optional (default: 10)
+        Number of CUDA streams to utilize.
+    noverlap: int, optional (default: 10)
+        Number of overlapping bins to use
+    alpha: float (> 1), optional, (default: 1.5)
+        1 + dlog q, where dlog q = dq / q
+    block_size: int, optional (default: 256)
+        CUDA block size to use (must be power of 2)
+    batch_size: int, optional (default: 1)
+        Number of frequencies to compute in a single batch
+
+    Returns
+    -------
+    bls: array_like, float
+        BLS periodogram, normalized to 1 - chi2(best_fit) / chi2(constant)
+    qphi_sols: list of (q, phi) tuples
+        Best (q, phi) solution at each frequency
+
+    """
+
+    # Read kernel
+    kernel_txt = _module_reader(find_kernel('bls'),
+                                cpp_defs=dict(BLOCK_SIZE=block_size))
+
+    # compile kernel
+    module = SourceModule(kernel_txt, options=['--use_fast_math'])
+
+    # get functions
+    gpu_bls = module.get_function('binned_bls_bst')
+    gpu_max = module.get_function('reduction_max')
+    gpu_bin = module.get_function('bin_and_phase_fold_bst_multifreq')
+
+    # Prepare the functions
+    gpu_bls = gpu_bls.prepare([np.intp, np.intp, np.intp, np.int32])
+    gpu_max = gpu_max.prepare([np.intp, np.intp, np.int32, np.int32, np.int32,
+                               np.intp, np.intp, np.int32, np.int32])
+    gpu_bin = gpu_bin.prepare([np.intp, np.intp, np.intp, np.intp,
+                               np.intp, np.intp, np.int32, np.int32,
+                               np.int32, np.int32, np.int32, np.int32,
+                               np.float32, np.int32])
+
+    # smallest and largest number of bins
+    nbins0_max = 1
+    nbinsf_max = 1
+
+    qmin_all = qmin_fac * q_transit(min(freqs), rho)
+    qmin_max_all = (qmax_fac / qmin_fac) * qmin_all
+    while (int(1./(qmin_max_all)) > nbins0_max):
+        nbins0_max = np.ceil(alpha * nbins0_max)
+
+    while (int(1./(qmin_all)) > nbinsf_max):
+        nbinsf_max = np.ceil(alpha * nbinsf_max)
+
+    ndata = np.int32(len(t))
+
+    q_phi = get_qphi(nbins0_max, nbinsf_max, alpha, noverlap)
+    # qvals, phivals = zip(*q_phi)
+
+    nbins_tot = len(q_phi) / noverlap
+
+    gs = batch_size * len(q_phi)
+
+    grid_size = int(np.ceil(float(gs) / block_size))
+
+    w = np.power(dy, -2)
+    w /= sum(w)
+    ybar = np.dot(w, y)
+    YY = np.dot(w, np.power(y - ybar, 2))
+
+    t_g = gpuarray.to_gpu(np.array(t).astype(np.float32))
+    yw_g = gpuarray.to_gpu(np.array((y - ybar) * w).astype(np.float32))
+    w_g = gpuarray.to_gpu(np.array(w).astype(np.float32))
+    freqs_g = gpuarray.to_gpu(np.array(freqs).astype(np.float32))
+
+    yw_g_bins, w_g_bins, bls_tmp_gs, bls_tmp_sol_gs, streams \
+        = [], [], [], [], []
+    for i in range(nstreams):
+        streams.append(cuda.Stream())
+        yw_g_bins.append(gpuarray.zeros(gs, dtype=np.float32))
+        w_g_bins.append(gpuarray.zeros(gs, dtype=np.float32))
+        bls_tmp_gs.append(gpuarray.zeros(gs, dtype=np.float32))
+        bls_tmp_sol_gs.append(gpuarray.zeros(gs, dtype=np.int32))
+
+    bls_g = gpuarray.zeros(len(freqs), dtype=np.float32)
+    bls_sol_g = gpuarray.zeros(len(freqs), dtype=np.int32)
+
+    block = (block_size, 1, 1)
+
+    grid = (grid_size, 1)
+
+    nbatches = int(np.ceil(float(len(freqs)) / batch_size))
+    for batch in range(nbatches):
+
+        qmin = qmin_fac * q_transit(freqs[batch_size * batch], rho)
+        imax = min([len(freqs) - 1, batch_size * (batch + 1) - 1])
+        qmax = qmax_fac * q_transit(freqs[imax], rho)
+
+        nbins0 = 1
+        nbinsf = 1
+        while (int(1./(qmin)) > nbins0_max):
+            nbins0 = np.ceil(alpha * nbins0)
+
+        while (int(1./(qmin)) > nbinsf_max):
+            nbinsf = np.ceil(alpha * nbinsf)
+
+        # q_phi = get_qphi(nbins0_max, nbinsf_max, alpha, noverlap)
+        # qvals, phivals = zip(*q_phi)
+
+        nbins_tot = len(q_phi) / noverlap
+
+        nf = batch_size if batch < nbatches - 1 else \
+            len(freqs) - batch * batch_size
+
+        nf = np.int32(nf)
+        j = batch % nstreams
+        yw_g_bin = yw_g_bins[j]
+        w_g_bin = w_g_bins[j]
+        bls_tmp_g = bls_tmp_gs[j]
+        bls_tmp_sol_g = bls_tmp_sol_gs[j]
+
+        stream = streams[j]
+
+        yw_g_bin.fill(np.float32(0), stream=stream)
+        w_g_bin.fill(np.float32(0), stream=stream)
+
+        bin_grid = (int(np.ceil(float(ndata * nf) / block_size)), 1)
+
+        args = (bin_grid, block, stream)
+        args += (t_g.ptr, yw_g.ptr, w_g.ptr)
+        args += (yw_g_bin.ptr, w_g_bin.ptr, freqs_g.ptr)
+        args += (ndata, nf, np.int32(nbins0), np.int32(nbinsf))
+        args += (np.int32(batch_size * batch), noverlap)
+        args += (alpha, nbins_tot)
+        gpu_bin.prepared_async_call(*args)
+
+        args = (grid, block, stream)
+        args += (yw_g_bin.ptr, w_g_bin.ptr)
+        args += (bls_tmp_g.ptr,  nf * nbins_tot * noverlap)
+        gpu_bls.prepared_async_call(*args)
+
+        bls_tmp = None if not plot_status else bls_tmp_g.get()
+
+        args = (gpu_max, bls_tmp_g, bls_tmp_sol_g)
+        args += (nf, nbins_tot * noverlap, stream, bls_g, bls_sol_g)
+        args += (batch * batch_size, block_size)
+        reduction_max(*args)
+
+    bls_sols = bls_sol_g.get()
+
+    assert(not any(bls_sols < 0))
+    qphi_sols = [(qvals[b], phivals[b]) for b in bls_sols]
+
+    return bls_g.get()/YY, qphi_sols
--- cuvarbase/wavelet.py
+++ cuvarbase/wavelet.py
@@ -1,108 +1,109 @@
-import numpy as np 
+import numpy as np
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
 from pycuda.compiler import SourceModule
-import resource 
+import resource
 from .core import GPUAsyncProcess, BaseSpectrogram
 from .utils import weights, find_kernel
-
 from time import time
+
+
 class Wavelet(object):
-	def __init__(self, t, y, dy, pmin=0.05, pmax=15.0, sigma=0.05, 
-					precision=8, samples_per_peak=5, resolution_enhancement=1.,
-					block_size=128):
-		self.t = t
-		self.y = y
-		self.dy = dy
+    def __init__(self, t, y, dy, pmin=0.05, pmax=15.0, sigma=0.05, 
+                    precision=8, samples_per_peak=5, resolution_enhancement=1.,
+                    block_size=128):
+        self.t = t
+        self.y = y
+        self.dy = dy
 
-		self.sigma = sigma
-		self.precision = precision
-		self.samples_per_peak = samples_per_peak
-		self.pmin = pmin
-		self.pmax = pmax
-		self.block_size=block_size
-		self.resolution_enhancement = resolution_enhancement
+        self.sigma = sigma
+        self.precision = precision
+        self.samples_per_peak = samples_per_peak
+        self.pmin = pmin
+        self.pmax = pmax
+        self.block_size=block_size
+        self.resolution_enhancement = resolution_enhancement
 
-		self.dlnf = 7.32E-3 * (5./samples_per_peak) * (sigma / 0.05) * (8./precision)**0.5
+        self.dlnf = 7.32E-3 * (5./samples_per_peak) * (sigma / 0.05) * (8./precision)**0.5
 
-		self.nfreqs = int(np.ceil(np.log(pmax / pmin) / self.dlnf))
+        self.nfreqs = int(np.ceil(np.log(pmax / pmin) / self.dlnf))
 
-		self.freqs = np.logspace(np.log10(1./pmax), np.log10(1./pmin), self.nfreqs)
+        self.freqs = np.logspace(np.log10(1./pmax), np.log10(1./pmin), self.nfreqs)
 
-		print("WAVELET: making taus")
-		t0 = time()
-		self.taus =  []
-		for f in self.freqs:
-			
-			# 0.5 = exp(-(sigma * omega * (width/2))^2)
-			dtau = np.sqrt(np.log(2)) / ( np.pi * self.sigma * f * self.resolution_enhancement)
+        print("WAVELET: making taus")
+        t0 = time()
+        self.taus = []
+        for f in self.freqs:
+            
+            # 0.5 = exp(-(sigma * omega * (width/2))^2)
+            dtau = np.sqrt(np.log(2)) / ( np.pi * self.sigma * f * self.resolution_enhancement)
 
-			ntaus = int(np.ceil((max(t) - min(t)) / dtau))
-			taus = np.linspace(min(t), max(t), ntaus).tolist()
-			taus.append(max(t))
-			self.taus.append(taus)
+            ntaus = int(np.ceil((max(t) - min(t)) / dtau))
+            taus = np.linspace(min(t), max(t), ntaus).tolist()
+            taus.append(max(t))
+            self.taus.append(taus)
 
-			
-		print(time() - t0)
-		self.prepared_functions = {}
-		print("WAVELET: preparing/compiling functions")
-		t0 = time()
-		self._compile_and_prepare_functions()
-		print(time() - t0)
+            
+        print(time() - t0)
+        self.prepared_functions = {}
+        print("WAVELET: preparing/compiling functions")
+        t0 = time()
+        self._compile_and_prepare_functions()
+        print(time() - t0)
 
-	def _compile_and_prepare_functions(self, **kwargs):
+    def _compile_and_prepare_functions(self, **kwargs):
 
-		self.module = SourceModule(open(find_kernel('wavelet'), 'r').read(), 
-										options=['--use_fast_math'])
+        self.module = SourceModule(open(find_kernel('wavelet'), 'r').read(), 
+                                        options=['--use_fast_math'])
 
-		self.dtypes = dict(
-			wavelet_spectrogram =[ np.intp, np.intp, np.intp, np.intp, np.intp, np.intp, 
-									np.intp, np.int32, np.int32, np.float32, np.float32]
-		)
+        self.dtypes = dict(
+            wavelet_spectrogram =[ np.intp, np.intp, np.intp, np.intp, np.intp, np.intp, 
+                                    np.intp, np.int32, np.int32, np.float32, np.float32]
+        )
 
-		for function, dtype in self.dtypes.iteritems():
-			self.prepared_functions[function] = self.module.get_function(function).prepare(dtype)
+        for function, dtype in self.dtypes.iteritems():
+            self.prepared_functions[function] = self.module.get_function(function).prepare(dtype)
 
 #float *t, float *y, float *w, float *spectrogram, 
 #float *freqs, float *taus, int *ntaus, int nfreqs, 
 #int nobs, float sigma, float prec
-	def run(self):
-		ntaus = [ len(taus) for taus in self.taus ]
-		npxls = sum(ntaus)
+    def run(self):
+        ntaus = [ len(taus) for taus in self.taus ]
+        npxls = sum(ntaus)
 
 
-		spectrogram_g = gpuarray.zeros(npxls, dtype=np.float32)
-		ntaus_g = gpuarray.to_gpu(np.array(ntaus, dtype=np.int32))
-		t_g = gpuarray.to_gpu(np.array(self.t, dtype=np.float32))
-		y_g = gpuarray.to_gpu(np.array(self.y, dtype=np.float32))
+        spectrogram_g = gpuarray.zeros(npxls, dtype=np.float32)
+        ntaus_g = gpuarray.to_gpu(np.array(ntaus, dtype=np.int32))
+        t_g = gpuarray.to_gpu(np.array(self.t, dtype=np.float32))
+        y_g = gpuarray.to_gpu(np.array(self.y, dtype=np.float32))
 
-		w = np.power(self.dy, -2)
-		w_g = gpuarray.to_gpu(np.array(w / np.median(w), dtype=np.float32))
+        w = np.power(self.dy, -2)
+        w_g = gpuarray.to_gpu(np.array(w / np.median(w), dtype=np.float32))
 
-		sig_g = np.float32(self.sigma)
-		prec_g = np.float32(self.precision)
-		nobs_g = np.int32(len(self.t))
-		nfreqs_g = np.int32(len(self.freqs))
+        sig_g = np.float32(self.sigma)
+        prec_g = np.float32(self.precision)
+        nobs_g = np.int32(len(self.t))
+        nfreqs_g = np.int32(len(self.freqs))
 
-		freqs_g = gpuarray.to_gpu(np.array(self.freqs, dtype=np.float32))
+        freqs_g = gpuarray.to_gpu(np.array(self.freqs, dtype=np.float32))
 
-		taus_flat = []
-		for taus in self.taus:
-			taus_flat.extend(list(taus))
-		taus_g = gpuarray.to_gpu(np.array(taus_flat, dtype=np.float32))
+        taus_flat = []
+        for taus in self.taus:
+            taus_flat.extend(list(taus))
+        taus_g = gpuarray.to_gpu(np.array(taus_flat, dtype=np.float32))
 
 
-		block = (self.block_size, 1, 1)
-		grid = (int(np.ceil(float(npxls) / self.block_size)), 1) 
-		self.prepared_functions['wavelet_spectrogram'].prepared_call(grid, block,
-			t_g.ptr, y_g.ptr, w_g.ptr, spectrogram_g.ptr, freqs_g.ptr, taus_g.ptr, ntaus_g.ptr, nfreqs_g, 
-			nobs_g, sig_g, prec_g)
+        block = (self.block_size, 1, 1)
+        grid = (int(np.ceil(float(npxls) / self.block_size)), 1) 
+        self.prepared_functions['wavelet_spectrogram'].prepared_call(grid, block,
+            t_g.ptr, y_g.ptr, w_g.ptr, spectrogram_g.ptr, freqs_g.ptr, taus_g.ptr, ntaus_g.ptr, nfreqs_g, 
+            nobs_g, sig_g, prec_g)
 
-		self.spectrogram = spectrogram_g.get()
+        self.spectrogram = spectrogram_g.get()
 
-		return self.spectrogram
+        return self.spectrogram
 
 
 
 #class Wavelet(object):
-#	def __init__(self, t, y, dy, sigma=0.05, samples_per_peak=5, precision=8, )
+#   def __init__(self, t, y, dy, sigma=0.05, samples_per_peak=5, precision=8, )
