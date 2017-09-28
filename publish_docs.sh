916206a1b250ba090ba38be541c39de44f92ee3f
John Hoffman <jah5@princeton.edu>
2017-09-28 14:19:48 -0400

    added comments to publish_docs

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#
+# A hack-ish way to automate the document publishing process for
+# github pages.
+#
+# This won't work if you're not @johnh2o2 on Github.
+#
+# To build docs locally
+# ---------------------
+# Just ``cd docs && make html``. Then open docs/build/html/index.html.
 set -x
 
 DOC_BRANCH=devel
@@ -6,7 +16,7 @@ NEEDED="cuvarbase docs/Makefile docs/source README.rst INSTALL.rst CHANGELOG.rst
 # We need to grab hidden files with mv...
 shopt -s dotglob nullglob
 
-
+# Create gh-pages branch if one doesn't already exist.
 HAS_GH_BRANCH=`git branch | grep gh-pages`
 if [ "$HAS_GH_BRANCH" == "" ]; then
     echo "Did not detect gh-pages branch. Creating now."
@@ -41,4 +51,5 @@ git add --all
 git commit -m "Updating docs"
 git push -u origin gh-pages
 
+# go home
 git checkout $DOC_BRANCH
